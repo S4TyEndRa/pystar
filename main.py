7374db8afcaee1d96dcdd00ecd557169b9996e88
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-04-09 21:58:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/StarkBotsIndustries/RenameTelegramBot && cd RenameTelegramBot && python3 rename.py && https://github.com/StarkBotsIndustries/StringSessionBot && cd StringSessionBot && python3 generator.py && git clone https://github.com/StarkBotsIndustries/ChannelBot && cd ChannelBot && python3 channelbot.py && git clone https://github.com/StarkBotsIndustries/InstagramBot && cd InstagramBot && python3 insta.py && git clone https://github.com/StarkBotsIndustries/TimeBot && cd TimeBot && python3 bot.py")
